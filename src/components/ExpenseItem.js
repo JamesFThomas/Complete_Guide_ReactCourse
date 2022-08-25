64fdb23e0cf7bfa6128438ec6f105c754a506d4f
JamesFThomas <james.fthomas@yahoo.com>
2022-08-25 16:45:59 -0500

    change title btn removed from ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,12 @@
-import { useState } from 'react';
+import React from 'react';
 import '../style/ExpenseItem.css';
 
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (expenses) => {
-  const [title, setTitle] = useState(expenses.title);
-
   const date = expenses.date;
-  // console.log(typeof date);
-  //   const title = expenses.title;
+  const title = expenses.title;
   const amount = expenses.amount;
-
-  const clickHandler = () => {
-    setTitle('Updated');
-    console.log(title);
-  };
 
   return (
     <div className='expense-item'>
@@ -23,7 +15,6 @@ const ExpenseItem = (expenses) => {
         <h2> {title}</h2>
       </div>
       <div className='expense-item__price'> {amount} </div>
-      {/* <button onClick={clickHandler}>Change Title</button> */}
     </div>
   );
 };
