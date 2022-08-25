1d09dc5c9396bd404cafdd82d9c51cb568675b51
JamesFThomas <james.fthomas@yahoo.com>
2022-08-25 09:43:35 -0500

    dynamically render expenses array with map func

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -6,6 +6,8 @@ import ExpensesFilter from './ExpensesFilter';
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
 
+  const { expenses } = props; // destructure expenses array from props
+
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
@@ -15,26 +17,12 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        date={props.expenses[0].date}
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-      />
-      <ExpenseItem
-        date={props.expenses[1].date}
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-      />{' '}
-      <ExpenseItem
-        date={props.expenses[2].date}
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-      />
-      <ExpenseItem
-        date={props.expenses[3].date}
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-      />
+      {
+        // dynamically render all items in the expenses array
+        expenses.map((e) => (
+          <ExpenseItem date={e.date} title={e.title} amount={e.amount} />
+        ))
+      }
     </div>
   );
 };
